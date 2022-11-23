ba944d9c4a29724cce04930374a78c1bc21e93d5
Cyrus Cheng <cyrus@cyrus822.com>
2022-11-23 12:26:48 +0800

    Change sending method

--- ServiceBusDemo/DemoSender/src/main/java/com/cyrus822/manulife/messagingdemo/ServiceBusDemo/DemoSender/controllers/SessionSender.java
+++ ServiceBusDemo/DemoSender/src/main/java/com/cyrus822/manulife/messagingdemo/ServiceBusDemo/DemoSender/controllers/SessionSender.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.azure.core.util.BinaryData;
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusMessage;
-import com.azure.messaging.servicebus.ServiceBusMessageBatch;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import com.cyrus822.manulife.messagingdemo.ServiceBusDemo.DemoSender.models.Payment;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -37,46 +36,27 @@ public class SessionSender {
     private static final String OBJECTTYPE = "com.cyrus822.manulife.messagingdemo.ServiceBusDemo.DemoReceiver.models.Payment";    
 
     /* Without Session Id */
-    @PostMapping("/withoutSessionId")
-    public String withoutSessionId() {
+    @PostMapping("/noSessionId")
+    public String noSessionId() {
         String rtnMsg = "";
                 
         try{
             //prepare the sender
             ServiceBusSenderClient senderClient = new ServiceBusClientBuilder().connectionString(connStr).sender().queueName(noSessionQueueName).buildClient();
-            
+
             //prepare the message
-            ServiceBusMessageBatch messageBatch = senderClient.createMessageBatch(); 
-            
             for(int i=1;i<=30;i++){
-                Payment payment = new Payment(i, "001", "HKD", "76543210", 43.21);
+                Payment payment = new Payment(i, "012", "HKD", "01234567", 12.34);
                 String paymentJSON = new ObjectMapper().writeValueAsString(payment);
                 ServiceBusMessage msg = new ServiceBusMessage(BinaryData.fromBytes(paymentJSON.getBytes(UTF_8)));
                 Map<String, Object> maps = msg.getApplicationProperties();
                 maps.put("_type", OBJECTTYPE);
 
-                if (messageBatch.tryAddMessage(msg)) {
-                    continue;
-                }
+                senderClient.sendMessage(msg);
 
-                // The batch is full, so we create a new batch and send the batch.
-                senderClient.sendMessages(messageBatch);
-                System.out.println("Sent a batch of messages to the queue: " + noSessionQueueName);  
-                
-                // create a new batch
-                messageBatch = senderClient.createMessageBatch();
-
-                // Add that message that we couldn't before.
-                if (!messageBatch.tryAddMessage(msg)) {
-                    System.err.printf("Message is too large for an empty batch. Skipping. Max size: %s.", messageBatch.getMaxSizeInBytes());
-                }                
+                System.out.println(String.format("Policy {%d} send to {%s} without Session Id Success", payment.getPolicyNo(), noSessionQueueName));
             }
 
-            if (messageBatch.getCount() > 0) {
-                senderClient.sendMessages(messageBatch);
-                System.out.println("Sent a batch of messages to the queue: " + noSessionQueueName);
-            }
-        
             //close the client
             senderClient.close();
 
@@ -98,41 +78,22 @@ public class SessionSender {
             ServiceBusSenderClient senderClient = new ServiceBusClientBuilder().connectionString(connStr).sender().queueName(sessionQueueName).buildClient();
             
             //prepare the message
-            ServiceBusMessageBatch messageBatch = senderClient.createMessageBatch(); 
-            
             for(int i=1;i<=30;i++){
-                Payment payment = new Payment(i, "012", "USD", "01234567", 12.34);
+                Payment payment = new Payment(i, "001", "USD", "7654321", 43.21);
                 String paymentJSON = new ObjectMapper().writeValueAsString(payment);
                 ServiceBusMessage msg = new ServiceBusMessage(BinaryData.fromBytes(paymentJSON.getBytes(UTF_8))).setSessionId("ctx-" + Integer.toString(payment.getPolicyNo()%10));
                 Map<String, Object> maps = msg.getApplicationProperties();
                 maps.put("_type", OBJECTTYPE);
 
-                if (messageBatch.tryAddMessage(msg)) {
-                    continue;
-                }
+                senderClient.sendMessage(msg);
 
-                // The batch is full, so we create a new batch and send the batch.
-                senderClient.sendMessages(messageBatch);
-                System.out.println("Sent a batch of messages to the queue: " + sessionQueueName);  
-                
-                // create a new batch
-                messageBatch = senderClient.createMessageBatch();
-
-                // Add that message that we couldn't before.
-                if (!messageBatch.tryAddMessage(msg)) {
-                    System.err.printf("Message is too large for an empty batch. Skipping. Max size: %s.", messageBatch.getMaxSizeInBytes());
-                }                
+                System.out.println(String.format("Policy {%d} send to {%s} with Session Id Success", payment.getPolicyNo(), sessionQueueName));
             }
 
-            if (messageBatch.getCount() > 0) {
-                senderClient.sendMessages(messageBatch);
-                System.out.println("Sent a batch of messages to the queue: " + sessionQueueName);
-            }
-        
             //close the client
             senderClient.close();
 
-            rtnMsg = String.format("Send to {%s} without Session Id Success", sessionQueueName);
+            rtnMsg = String.format("Send to {%s} with Session Id Success", sessionQueueName);
         } catch (Exception e){
             rtnMsg = "Send Fail" + e.getStackTrace();
             e.printStackTrace();
